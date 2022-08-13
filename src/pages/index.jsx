6a33724f8caa41fbd035b51e9d0e5531c6650f49
João Vitor F <joaovitor.jobs2001@gmail.com>
2022-08-13 20:20:43 -0300

    feat: added get all tasks functionality

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import { Header } from '../components/Header'
 import { User } from '../components/User'
 
-const HomePage = () => {
+const HomePage = ({ users }) => {
   return (
     <div>
       <Head>
@@ -15,11 +15,24 @@ const HomePage = () => {
       <div className="w-full h-full">
         <Header />
         <main className="max-w-5xl mx-auto h-full py-8 grid grid-cols-2 gap-4">
-          <User />
+          {users.map(user => (
+            <User key={user.id} user={user} />
+          ))}
         </main>
       </div>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const users = await res.json()
+
+  return {
+    props: {
+      users,
+    },
+  }
 }
 
 export default HomePage
